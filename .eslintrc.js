58bae522aa5a679216f59ea2d69d4efc5c89276b
Diego Vargas <diego@soho.cl>
2020-05-15 15:38:16 -0500

    add coreui nuxt

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,16 +8,17 @@ module.exports = {
     parser: 'babel-eslint'
   },
   extends: [
-    '@nuxtjs',
-    'prettier',
-    'prettier/vue',
-    'plugin:prettier/recommended',
-    'plugin:nuxt/recommended'
+    'plugin:vue/recommended',
+    'plugin:prettier/recommended'
   ],
+  // required to lint *.vue files
   plugins: [
+    'vue',
     'prettier'
   ],
   // add your custom rules here
   rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   }
 }
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,63 +1,81 @@
-export default {
+const pkg = require('./package')
+
+module.exports = {
   mode: 'universal',
+
   /*
-   ** Headers of the page
-   */
+  ** Headers of the page
+  */
   head: {
-    title: process.env.npm_package_name || '',
+    title: pkg.name,
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      {
-        hid: 'description',
-        name: 'description',
-        content: process.env.npm_package_description || ''
-      }
+      { hid: 'description', name: 'description', content: pkg.description }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'https://use.fontawesome.com/releases/v5.7.2/css/all.css' }
+    ]
   },
+
   /*
-   ** Customize the progress-bar color
-   */
-  loading: { color: '#fff' },
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#42A5CC' },
+
   /*
-   ** Global CSS
-   */
-  css: ['element-ui/lib/theme-chalk/index.css'],
-  /*
-   ** Plugins to load before mounting the App
-   */
-  plugins: ['@/plugins/element-ui'],
-  /*
-   ** Nuxt.js dev-modules
-   */
-  buildModules: [
-    // Doc: https://github.com/nuxt-community/eslint-module
-    '@nuxtjs/eslint-module'
+  ** Global CSS
+  */
+  css: [
+    /* Import Font Awesome Icons Set */
+    '~/node_modules/flag-icon-css/css/flag-icon.min.css',
+    /* Import Simple Line Icons Set */
+    '~/node_modules/simple-line-icons/css/simple-line-icons.css',
+    /* Import Core SCSS */
+    '@/assets/scss/style.scss',
+    /* Import Custom CSS */
+    '@/assets/css/main.css'
   ],
+
   /*
-   ** Nuxt.js modules
-   */
+  ** Plugins to load before mounting the App
+  */
+  plugins: [],
+
+  /*
+  ** Nuxt.js modules
+  */
   modules: [
-    // Doc: https://axios.nuxtjs.org/usage
+    // Doc: https://github.com/nuxt-community/axios-module#usage
     '@nuxtjs/axios',
-    '@nuxtjs/pwa',
-    // Doc: https://github.com/nuxt-community/dotenv-module
-    '@nuxtjs/dotenv'
+    // Doc: https://bootstrap-vue.js.org/docs/
+    'bootstrap-vue/nuxt'
   ],
   /*
-   ** Axios module configuration
-   ** See https://axios.nuxtjs.org/options
-   */
-  axios: {},
+  ** Axios module configuration
+  */
+  axios: {
+    // See https://github.com/nuxt-community/axios-module#options
+  },
+
   /*
-   ** Build configuration
-   */
+  ** Build configuration
+  */
   build: {
-    transpile: [/^element-ui/],
     /*
-     ** You can extend webpack config here
-     */
-    extend(config, ctx) {}
+    ** You can extend webpack config here
+    */
+    extend(config, ctx) {
+      // Run ESLint on save
+      if (ctx.isDev && ctx.isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
   }
 }
